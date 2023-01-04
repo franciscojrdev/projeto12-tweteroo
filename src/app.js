023637c8de1343313d2b480b848598f98c83dee8
franciscojrdev <franciscojunior7222@gmail.com>
2023-01-04 17:03:32 -0300

    feat: sign-up and tweets routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,26 @@ import express from 'express'
 
 
 const app = express()
+app.use(express.json())
+
+const usuario = []
+const tweets = []
+
+app.post("/sign-up", (req,res) => {
+     const {username, avatar} = req.body
+     if(!username || !avatar){
+        res.send("Erro").status(400)
+        return
+     }
+     usuario.push({username,avatar})
+     console.log(usuario)
+     res.send(usuario)
+})
+
+app.post("/tweets", (req,res) =>{
+    const {username,tweet} = req.body
+    if()
+})
 
 app.listen(5000,() => {
     console.log("Server running in port 5000")
