8b4d6d0f4056f1a4096ff7b1dc24fcfc422fdd9e
franciscojrdev <franciscojunior7222@gmail.com>
2023-01-04 23:04:25 -0300

    feat:  get tweet function done

--- src/app.js
+++ src/app.js
@@ -15,12 +15,25 @@ app.post("/sign-up", (req,res) => {
      }
      usuario.push({username,avatar})
      console.log(usuario)
-     res.send(usuario)
+     res.send("OK").status(200)
 })
 
 app.post("/tweets", (req,res) =>{
     const {username,tweet} = req.body
-    if()
+    if(!username || !tweet){
+        res.sendStatus(400)
+        return
+    }
+    tweets.push({username,tweet})
+    res.send("OK").status(200)
+})
+
+app.get("/tweets", (req,res) =>{
+    if(!tweets){
+        res.send("Erro").status(400)
+        return
+    }
+    res.send(tweets.reverse())
 })
 
 app.listen(5000,() => {
