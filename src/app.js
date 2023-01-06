8f7c788b70bb339b39914dc8bc6815f64217870a
franciscojrdev <franciscojunior7222@gmail.com>
2023-01-06 00:09:30 -0300

    feat: tweets view and reverse function from tweets

--- src/app.js
+++ src/app.js
@@ -1,41 +1,43 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
+const usuario = [];
+const tweets = [];
 
-const usuario = []
-const tweets = []
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  if (!username || !avatar) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+  usuario.push({ username, avatar });
+  //  console.log(usuario)
+  res.send("OK").status(200);
+});
 
-app.post("/sign-up", (req,res) => {
-     const {username, avatar} = req.body
-     if(!username || !avatar){
-        res.send("Erro").status(400)
-        return
-     }
-     usuario.push({username,avatar})
-     console.log(usuario)
-     res.send("OK").status(200)
-})
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  if (!username || !tweet) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+  tweets.push({ username, tweet });
+  res.status(200).send("OK");
+});
 
-app.post("/tweets", (req,res) =>{
-    const {username,tweet} = req.body
-    if(!username || !tweet){
-        res.sendStatus(400)
-        return
-    }
-    tweets.push({username,tweet})
-    res.send("OK").status(200)
-})
+app.get("/tweets", (req, res) => {
+  const { avatar } = req.headers;
+  if (!tweets || !avatar) {
+    res.status(400).send("Erro");
+    return;
+  }
+  const arrayTweets = tweets.reverse().slice(0, 10)
+});
 
-app.get("/tweets", (req,res) =>{
-    if(!tweets){
-        res.send("Erro").status(400)
-        return
-    }
-    res.send(tweets.reverse())
-})
-
-app.listen(5000,() => {
-    console.log("Server running in port 5000")
-})
+app.listen(5000, () => {
+  console.log("Server running in port 5000");
+});
